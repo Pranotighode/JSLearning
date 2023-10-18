01adc4a77951841376fcd24d6733ee4e82fc5bc3
Pranotighode <pranotighode@gmail.com>
2023-10-18 21:18:26 +0530

    variable assignment
    variable assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log(myFullName);
+myFullName = 'Pranoti Ghode'
+console.log(myFullName);
+myFullName = 'Pranoti Sanjay Ghode'
+console.log(myFullName);
+
+var myLove = 'i love only JS';
+console.log(myLove);
+
+var firstName ="Pranoti"
+console.log(firstName);
+var lastName ="Ghode"
+console.log(lastName);
+var age = "21"
+console.log(age);
+var address= "Pandharpur"
+console.log(address);
+var pinCode ="413304"
+console.log(pinCode);
+var gender ="Female"
+console.log(gender);
+var isMarried="No"
+console.log(isMarried);
