39b979004b12c8686e92d3ceb58c778282fb1b31
Pranotighode <pranotighode@gmail.com>
2023-10-18 21:17:20 +0530

    variable practice
    variable practice

---
+++ 01_variable.js
@@ -0,0 +1,25 @@
+var firstName; // variable declaration
+
+firstName = "Pranoti"; // Variable initialization
+
+console.log(firstName);
+
+firstName = "Miss.Pranoti"; // Variable update
+
+console.log(firstName);
+
+var lastName = "Ghode"; // variable declaration and initialization
+
+console.log(lastName);
+
+var height; // Variable Declaration
+
+console.log(height);
+
+height = "6"; // initialization
+
+console.log(height);
+
+height = "6.2 feet"; // update
+
+console.log(height);
